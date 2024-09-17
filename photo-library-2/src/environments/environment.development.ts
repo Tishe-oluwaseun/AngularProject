aa44234110077eb15e43a4c14a41b798d7ae020f
Tishe-oluwaseun <oluwaseun@systemspecscomng.onmicrosoft.com>
2024-09-17 09:27:44 +0100

    Update environment.development.ts

--- photo-library-2/src/environments/environment.development.ts
+++ photo-library-2/src/environments/environment.development.ts
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 
 export const environment = {
   production: true,
-  apiKey: "AIzaSyD_-mvl0fTcCa7JhtTkgZV-Ofqv_LUpYS8",
+  apiKey: "",
   authDomain: "photo-library-2.firebaseapp.com",
   projectId: "photo-library-2",
   storageBucket: "photo-library-2.appspot.com",
   messagingSenderId: "517492012960",
-  appId: "1:517492012960:web:6d6dd4e538d3340eb856c5"
+  appId: ""
 };
 
 
